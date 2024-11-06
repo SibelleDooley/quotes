ece4046b82224679eaeaa07fc57c46574439c93c
SibelleDooley <sibelledooley@gmail.com>
2024-11-06 13:14:27 -0700

    Initial Commit

---
+++ Encouraging.js
@@ -0,0 +1,4 @@
+document.getElementById
+    function redirectToWebsite() {
+      window.location.href = "https://www.google.com/search?q=encouraging+quotes&rlz=1C1RXQR_enUS1126US1126&oq=Encouraging+quotes&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIGCAEQRRhAMgcIAhAAGIAEMgcIAxAAGIAEMgcIBBAAGIAEMgcIBRAAGIAEMgcIBhAAGIAEMgcIBxAAGIAE0gEKMTQ0MzVqMGoxNagCALACAA&sourceid=chrome&ie=UTF-8"; 
+    }
---
+++ Movies.js
@@ -0,0 +1,4 @@
+document.getElementById
+    function redirectToWebsite() {
+      window.location.href = "https://www.google.com/search?sca_esv=f5375ae535b801fb&rlz=1C1RXQR_enUS1126US1126&sxsrf=ADLYWIL3GJtTSCjH0oful99VuDaYoe2bBQ:1730672403553&q=movie+quotes&udm=2&fbs=AEQNm0BglSNKPbDQcL4Et01QEIYvJ5VGsHgUL_tsKqYywhWXkknveTpaLEIQiU02u5i1FK60ElrIW9FFtqgEnQbUPTP1v5PQAzhf1Y13Kdv-jba_5gxhg0vDqZDkhJc6r3gAp6AjEH3uQGUsTlbW9bCqWbU_KQmLRMpSvMOHQImtns6Bjg8mdovUCIgSWlsLlCl7eMPas0SlDN3m17qUidJPA2sLM55w-g&sa=X&ved=2ahUKEwi8ldiOmcGJAxVuke4BHegxKNIQtKgLegQIHBAB&biw=1536&bih=747&dpr=1.25"
+    }
